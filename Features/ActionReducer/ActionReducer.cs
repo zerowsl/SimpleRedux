f96b2b3c51cc4fbf75fd58e209bbe1c06f2c7b8e
zerowsl <zerowsl@users.noreply.github.com>
2017-11-30 00:26:28 +0800

    Add files via upload

---
+++ Features/ActionReducer/ActionReducer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleRedux
+{
+    public interface IDyAction
+    {
+        string Type { get; }
+        object Payload { get; }
+    }
+
+    public class DyAction<T> : IDyAction
+    {
+        public string Type { get; set; }
+        public T Payload { get; set; }
+
+        object IDyAction.Payload => Payload;
+    }
+
+    public static class DyAction
+    {
+        public static DyAction<object> Create(string type) => new DyAction<object> { Type = type };
+        public static DyAction<T> Create<T>(string type, T payload = default(T)) => new DyAction<T> { Type = type, Payload = payload };
+    }
+
+    public class ActionReducer<T>
+    {
+        readonly Dictionary<string, List<Reducer<T>>> dict = new Dictionary<string, List<Reducer<T>>>();
+
+        public Action OnAction<TAction>(Func<T, TAction, T> fn) => OnAction(typeof(TAction).FullName, fn);
+
+        public Action OnAction<TPayload>(string type, Func<T, TPayload, T> fn)
+        {
+            if (type == null) throw new ArgumentNullException(nameof(type));
+
+            if (!dict.TryGetValue(type, out var ls))
+                dict[type] = ls = new List<Reducer<T>>();
+
+            var h = AddToList(ls, (state, action) =>
+            {
+                if (fn == null) return state;
+                return fn(state, (TPayload)action);
+            });
+
+            return () =>
+            {
+                if (fn == null) return;
+                if (dict.TryGetValue(type, out ls))
+                {
+                    ls.Remove(h);
+                    if (ls.Count == 0) dict.Remove(type);
+                }
+                fn = null;
+            };
+        }
+
+        public T Reducer(T state, object action)
+        {
+            if (action != null)
+            {
+                string ty;
+                object a;
+                if (action is IDyAction d)
+                {
+                    ty = d.Type;
+                    a = d.Payload;
+                }
+                else //action should not to be null
+                {
+                    ty = action?.GetType().FullName;
+                    a = action;
+                }
+
+                if (ty != null && dict.TryGetValue(ty, out var ls) && ls != null)
+                {
+                    foreach (var fn in ls.ToArray())
+                        if (fn != null)
+                            state = fn(state, a);
+                }
+            }
+            return state;
+        }
+
+        static Titem AddToList<Titem>(List<Titem> list, Titem item)
+        {
+            list.Add(item);
+            return item;
+        }
+    }
+}
---
+++ Features/Middlewares.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SimpleRedux
+{
+    public static class Middlewares
+    {
+        private static readonly Task AsyncVoidTask = Task.FromResult<object>(null);
+
+        public static Task DispatchAsTask(this IStore store, object action) => store.Dispatch(action) as Task;
+        public static Task<T> DispatchAsTask<T>(this IStore store, object action) => store.Dispatch(action) as Task<T>;
+
+        public static Func<Dispatcher, Dispatcher> AsyncTask<T>(Dispatcher dispatch, Func<T> getState)
+        {
+            return next => action =>
+            {
+                object o = af<Func<Dispatcher, Dispatcher, Func<T>, Task>>(action, f => f(next, dispatch, getState));
+                o = o ?? af<Func<Dispatcher, Func<T>, Task>>(action, f => f(next, getState));
+                o = o ?? af<Func<Dispatcher, Task>>(action, f => f(next));
+                return o ?? next(action);
+            };
+        }
+
+        public static Func<Dispatcher, Dispatcher> Thunk<T>(Dispatcher dispatch, Func<T> getState)
+        {
+            return next => action =>
+            {
+                object o = af<Action<Dispatcher, Dispatcher, Func<T>>>(action, f => f(next, dispatch, getState), action);
+                o = o ?? af<Action<Dispatcher, Func<T>>>(action, f => f(next, getState), action);
+                o = o ?? af<Action<Dispatcher>>(action, f => f(next), action);
+                o = o ?? af<Func<Dispatcher, T>>(action, f => f(next));
+                o = o ?? af<Func<T, T>>(action, f => f(getState()));
+                return o ?? next(action);
+            };
+        }
+
+        private static object af<TDelegate>(object f, Func<TDelegate, object> ctodo) where TDelegate : class
+        {
+            if (f is TDelegate d) return ctodo(d) ?? AsyncVoidTask;
+            return null;
+        }
+
+        private static object af<TDelegate>(object a, Action<TDelegate> ctodo, object r) where TDelegate : class
+        {
+            if (!(a is TDelegate d)) return null;
+            ctodo(d);
+            return r;
+        }
+    }
+}
